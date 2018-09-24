9f121d840e941ee9a0f430907fa8f1226910e587
ksenia-gezalova <ksenia.gezalova@gmail.com>
2018-09-24 10:53:02 +0300

    dkv

--- js/setup.js
+++ js/setup.js
@@ -27,6 +27,7 @@ var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
 var wizardEyesValue = document.querySelector('[name="eyes-color"]');
 var fireball = document.querySelector('.setup-fireball-wrap');
 var fireballValue = document.querySelector('[name="fireball-color"]');
+var dialogHandler = userDialog.querySelector('.upload');
 
 
 var getRandomElement = function (arr) {
@@ -151,4 +152,52 @@ fireball.addEventListener('click', function () {
   var fireballColor = getRandomElement(FIREBALL_COLORS);
   fireball.style.background = fireballColor;
   fireballValue.value = fireballColor;
+});
+
+
+dialogHandler.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clentX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+    userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      };
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
